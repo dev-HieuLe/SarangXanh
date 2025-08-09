7da3663d68f162a6a33c72574a86e5ca214578af
HieuLe <152293787+pak-youu@users.noreply.github.com>
2025-08-09 11:02:19 +0700

    fix vercel and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
