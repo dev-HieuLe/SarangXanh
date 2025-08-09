2895602c016646c55fcd73b6fbc2f897dadf2c7d
HieuLe <152293787+pak-youu@users.noreply.github.com>
2025-08-09 10:35:28 +0700

    Update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // important for correct routing in Vercel
+  base: '/',
   plugins: [
     react(),
     tailwindcss()
