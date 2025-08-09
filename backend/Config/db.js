73bf62c1baf98a5c74082455ed53d51c60ca884f
HieuLe <152293787+pak-youu@users.noreply.github.com>
2025-08-09 22:11:41 +0700

    Update db.js -> Prevent db time out
    Use pool for db.
    Add Select 1 each 5 min prevent time out

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -2,13 +2,24 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = await mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
 });
+
+setInterval(async () => {
+  try {
+    await db.query("SELECT 1");
+    console.log("DB keep-alive ping successful");
+  } catch (error) {
+    console.error("DB keep-alive ping error:", error);
+  }
+}, 5 * 60 * 1000); // every 5 minutes
+
 
 export default db;
